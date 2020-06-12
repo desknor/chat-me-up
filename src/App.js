6e457740aef71ba20ea63426055a3c7ca94fe552
desknor <deschdev@gmail.com>
2020-06-12 17:03:44 -0400

    chatroom database setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import * as firebase from 'firebase';
+import RoomList from './components/RoomList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+var firebaseConfig = {
+  apiKey: "AIzaSyA-nNjNmFBfzBaPayxYRcol4YIEjLoiVYY",
+  authDomain: "chatmeup-cdbc3.firebaseapp.com",
+  databaseURL: "https://chatmeup-cdbc3.firebaseio.com",
+  projectId: "chatmeup-cdbc3",
+  storageBucket: "chatmeup-cdbc3.appspot.com",
+  messagingSenderId: "733200693495",
+  appId: "1:733200693495:web:42014729cbb86ac191fbf2",
+  measurementId: "G-WQLJD7Y873"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+class App extends Component {
+  
+    render() {
+      return (
+        <div className="App">
+          <header>
+            <h1>Chat-Me-Up</h1>
+          </header>
+          <main>
+            <section id="sidebar">
+              <RoomList 
+                firebase={firebase}
+              />
+            </section>
+          </main>
+        </div>
+      )
+    }
 }
-
 export default App;
